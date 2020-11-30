3db3275099febfa5879d310c3bee9c676220c20d
Silvija Gvazdaitytė <58749707+silvijaxoxo@users.noreply.github.com>
2020-11-30 02:57:48 +0200

    Update categories.component.js

--- module4-solution/src/menuapp/categories.component.js
+++ module4-solution/src/menuapp/categories.component.js
@@ -3,7 +3,7 @@
 
 angular.module('MenuApp')
 .component('categories', {
-  templateUrl: 'src/menuapp/templates/categoriesList.template.html',
+  templateUrl: 'src/menuapp/templates/categorieslist.template.html',
   bindings: {
     items: '<'
   }
